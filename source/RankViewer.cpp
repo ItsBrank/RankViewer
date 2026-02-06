e1d82a9f5fd7b0e9a991ac87207d91e39dc5fa63
BranK <23649425+ItsBranK@users.noreply.github.com>
2026-02-06 18:02:25 -0500

    Update RankViewer.cpp

--- source/RankViewer.cpp
+++ source/RankViewer.cpp
@@ -2,6 +2,11 @@
 // New Credits: Martin for the clean-up, thanks so much! I know it was a mess before, I suck at coding :D
 // New New Credits: I don't even know anymore so many people have helped, love this community everyone is so helpful <3
 
+// To Do:
+// - Convert rank pngs to tga for linux support (this one is easy at least).
+// - Fix aspect ratio being off for screens larger than 1920x1080p.
+// - Add support for the quads and heatseeker playlists.
+
 #include "pch.h"
 #include "RankViewer.h"
 #include "bakkesmod/wrappers/MMRWrapper.h"
